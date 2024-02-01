571c723efd03509ab02e6a1cc85268dd48793467
Theo Suquet <theo.suquet@live.fr>
2024-02-01 05:26:00 +0100

    Initial push

---
+++ realisation.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Théo Suquet</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+    <div id="header">
+        <div class="container">
+            <nav>
+                <img src="assets/logo.png" class="logo" alt="logo">
+                <ul>
+                    <li><a href="index.php">Accueil</a></li>
+                    <li><a href="entreprise.php">Entreprise</a></li>
+                    <li><a href="realisation.php">Réalisation</a></li>
+                    <li><a href="veille.php">Veille</a></li>
+                    <li><a href="bilan.php">Bilan</a></li>
+                </ul>
+            </nav>
+        </div>
+    </div>
+
+    <div id="portfolio">
+        <div class="container">
+            <div class="work-list">
+                <div class="work">
+                    <img src="assets/work.jpg" alt="work">
+                    <div class="layer">
+                        <h3>Project Name</h3>
+                        <p>Insert project description</p>
+                        <a href="#"><i class="fa-solid fa-arrow-up-right-from-square"></i></a>
+                    </div>
+                </div>
+                <div class="work">
+                    <img src="assets/work.jpg" alt="work">
+                </div>
+                <div class="work">
+                    <img src="assets/work.jpg" alt="work">
+                </div>
+                <div class="work">
+                    <img src="assets/work.jpg" alt="work">
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+
+</body>
+
+</html>
